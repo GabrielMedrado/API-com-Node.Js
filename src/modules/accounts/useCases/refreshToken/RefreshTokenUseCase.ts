4e20b9a626856c338121f191119bdd1613933e6a
Gabriel Medrado <medradoogabriel@gmail.com>
2022-08-18 17:20:14 -0300

    atualização

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
     email: string;
 }
 
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -18,7 +23,7 @@ class RefreshTokenUseCase {
         @inject('DayjsDateProvider')
         private dateProvider: IDateProvider,
     ) {}
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const { email, sub } = verify(
             token,
             auth.secret_refresh_token,
@@ -51,7 +56,15 @@ class RefreshTokenUseCase {
             user_id,
         });
 
-        return refresh_token;
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_refresh_token,
+        });
+
+        return {
+            refresh_token,
+            token: newToken,
+        };
     }
 }
 
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,48 +1,43 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { ensureAdmin } from "../middlewares/ensureAdmin";
-import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
-import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
-import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
-import uploadConfig from "@config/upload";
-import multer from "multer";
-
+import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
+import uploadConfig from '@config/upload';
+import multer from 'multer';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
-const listAvailableCarsController = new ListAvailableCarsController()
-const createCarSpecificationController = new CreateCarSpecificationController()
-const uploadCarImagesController = new UploadCarImagesController()
+const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
 
-const upload = multer(uploadConfig.upload("./tmp/cars"));
-
-
+const upload = multer(uploadConfig);
 
 carsRoutes.post(
-"/",
-ensureAuthenticated,
-ensureAdmin,
-createCarController.handle
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarController.handle,
 );
 
-carsRoutes.get("/available", listAvailableCarsController.handle);
+carsRoutes.get('/available', listAvailableCarsController.handle);
 
 carsRoutes.post(
-"/specifications/:id", 
-ensureAuthenticated,
-ensureAdmin, 
-createCarSpecificationController.handle
+    '/specifications/:id',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarSpecificationController.handle,
 );
 
 carsRoutes.post(
-"/images/:id", 
-upload.array("images"),
-uploadCarImagesController.handle
-)
+    '/images/:id',
+    upload.array('images'),
+    uploadCarImagesController.handle,
+);
 
-
-
-export { carsRoutes }
+export { carsRoutes };
