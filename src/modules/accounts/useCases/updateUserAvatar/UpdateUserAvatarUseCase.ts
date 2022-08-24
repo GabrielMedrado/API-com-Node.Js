6b2a563f33127d01d2ec47c3ed87f09fbeabc352
Gabriel Medrado <medradoogabriel@gmail.com>
2022-08-24 15:11:17 -0300

    Atualização

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,7 @@
+import { inject, injectable } from 'tsyringe';
+
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
-import { inject, injectable } from 'tsyringe';
 
 interface IRequest {
     user_id: string;
@@ -22,7 +23,6 @@ class UpdateUserAvatarUseCase {
         if (user.avatar) {
             await this.storageProvider.delete(user.avatar, 'avatar');
         }
-
         await this.storageProvider.save(avatar_file, 'avatar');
 
         user.avatar = avatar_file;
